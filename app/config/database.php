dd36b38c24e87af2098301441b86b45468c97cb8
John Mateo Aguilar <johnmateoaguilar@gmail.com>
2025-09-13 13:35:53 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql.freedb.tech',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'mockdata',
+    'username'	=> 'freedb_slq_database',
+    'password'	=> 'B9q@$qYp?KPujGb',
+    'database'	=> 'freedb_freesql_db',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
